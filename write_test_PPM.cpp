d33e92e5a2c813df25c5cbd64a0983cc38752fea
Rafael Santos <rafaels@gmail.com>
2019-10-08 17:32:28 -0400

    fixed code to write out pngs instead of ppm

--- write_test_PPM.cpp
+++ write_test_PPM.cpp
@@ -6,12 +6,18 @@
 #include "../stb/stb_image_write.h"
 
 int main(){
-    int nx = 200;
-    int ny = 100;
-    std::ofstream out_file;
-    out_file.open("test_image.ppm");
-    out_file << "P3\n" << nx << " " << ny << "\n255\n";
-    std::stringstream data;
+    const int nx = 200;
+    const int ny = 100;
+    const int chan_num = 3;
+    // std::ofstream out_file;
+    // out_file.open("test_image.ppm");
+    // out_file << "P3\n" << nx << " " << ny << "\n255\n";
+
+    // create array to hold data for png
+    const int data_size = nx * ny * chan_num;
+    unsigned char data[nx * ny * chan_num];
+    int index = 0;
+
     for(int j = ny - 1; j >= 0; j--){
         for(int i = 0; i < nx; i++){
             float r = float(i) / nx;
@@ -20,18 +26,19 @@ int main(){
             int ir = int(255.99 * r);
             int ig = int(255.99 * g);
             int ib = int(255.99 * b);
-            out_file << ir << " " << ig << " " << ib << std::endl;
-            data << ir << " " << ig << " " << ib << std::endl;
+            // out_file << ir << " " << ig << " " << ib << std::endl;
+            // data << ir << " " << ig << " " << ib << std::endl;
+            data[index++] = (unsigned char)(ir);
+            data[index++] = (unsigned char)(ig);
+            data[index++] = (unsigned char)(ib);
         }
     } 
-    out_file.close();
-    //unsigned char* datastr = NULL;
+    // out_file.close();
     //datastr = (unsigned char*)malloc(nx*ny*3);
-
     //data >> datastr;
     //std::cout << data.str();
-    std::string tmp = data.str();
+    // std::string tmp = data.str();
     //datastr = tmp.c_str();
-    stbi_write_png("test.png", nx, ny, 3, tmp.c_str(), 0);
+    stbi_write_png("test.png", nx, ny, chan_num, data, nx * chan_num);
     return 0;
 }
