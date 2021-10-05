ef59a4b802c22d3350404cd4436fd3100b957336
Rafael Santos <rafaels@gmail.com>
2021-10-05 16:08:58 -0400

    continued working on vector3 class

--- vec3.h
+++ vec3.h
@@ -37,5 +37,11 @@ public:
 };
 
 inline std::istream& operator>>(std::istream &is, vec3 &t){
-    is
+    is >> t.e[0] >> t.e[1] >> t.e[2];
+    return is;
+}
+
+inline std::ostream& operator<<(std::ostream &os, const vec3 &t){
+    os << t.e[0] << " " <<t.e[1] << " " << t.e[2];
+    return os;
 }
